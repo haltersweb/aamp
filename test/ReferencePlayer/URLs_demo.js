7c31d3c779c56634487a7d98d32666249b71b0f8
Adina Halter <adina_halter@cable.comcast.com>
2023-03-22 13:09:20 -0400

    added an encrypted demo sample and renamed the styles file

---
+++ test/ReferencePlayer/URLs_demo.js
@@ -0,0 +1,23 @@
+/*
+* If not stated otherwise in this file or this component's license file the
+* following copyright and licenses apply:
+*
+* Copyright 2018 RDK Management
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+var urls = [
+    { name:"Notre Dame Football", url: "https://ccr.s8live-nab.xcr.comcast.net/NABD_HD_30461_0_3046122222222221163.mpd", useDefaultDrmConfig: true },
+    { name:"Big Buck Bunny (CLEAR)", url: "http://amssamples.streaming.mediaservices.windows.net/683f7e47-bd83-4427-b0a3-26a6c4547782/BigBuckBunny.ism/manifest(format=mpd-time-csf)", useDefaultDrmConfig: false },
+];
