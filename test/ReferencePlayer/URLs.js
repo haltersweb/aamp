1f0bd6b556ba07456f81543b9621d1a312f3fa78
Adina Halter <adina_halter@cable.comcast.com>
2023-04-06 09:45:56 -0400

    misc changes from manan's Ref Player folder before I also make the changes to UVERefPlayer.js

--- test/ReferencePlayer/URLs.js
+++ test/ReferencePlayer/URLs.js
@@ -18,8 +18,9 @@
 */
 
 var urls = [
+     { name:"DERK_TEST", url: "http://10.131.157.158:8080/DERK_HD_TEST_123_0_0123456789012345163.mpd", useDefaultDrmConfig: false },
+     { name:"DRM DERK_TEST", url: "http://10.131.157.158:8081/DERK_HD_TEST_123_0_0123456789012345163.mpd", useDefaultDrmConfig: false },
+     { name:"Encrypted Asset", url: "https://ccr.s8live-nab.xcr.comcast.net/NABD_HD_30461_0_3046122222222221163.mpd", useDefaultDrmConfig: true },
      { name:"Big Buck Bunny (CLEAR)", url: "http://amssamples.streaming.mediaservices.windows.net/683f7e47-bd83-4427-b0a3-26a6c4547782/BigBuckBunny.ism/manifest(format=mpd-time-csf)", useDefaultDrmConfig: false },
-     { name:"Sintel MPD", url: "http://amssamples.streaming.mediaservices.windows.net/d444b1c9-c64a-4d65-b0f4-433810cf5e92/SintelMultiAudio.ism/manifest(format=mpd-time-csf)", useDefaultDrmConfig: false },
-     { name:"HLN_MIMIC (SCTE35)", url: "http://172.27.223.96:8880/HLN_MIMIC.mpd", useDefaultDrmConfig: true },
-     { name:"TRUTV_MIMIC (SCTE35)", url: "http://172.27.223.96:8880/TRUTV_MIMIC.mpd", useDefaultDrmConfig: true },
-];
+     { name:"Mortal Combat", url: "https://ccr.ipvod-t6-hd.xcr.comcast.net/ipvod7/TVNC0000000006252712/movie/1677080406242/manifest.mpd", useDefaultDrmConfig: true }
+ ];
